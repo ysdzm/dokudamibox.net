752c079435ae525d5bee0e59d92f999324872e62
dokudami <69578376+ysdzm@users.noreply.github.com>
2025-05-01 21:32:10 +0900

    fix: OGPに日本語が表示されるように修正 (#139)

--- src/pages/og-image/[slug].png.ts
+++ src/pages/og-image/[slug].png.ts
@@ -9,40 +9,59 @@ import { Resvg } from "@resvg/resvg-js";
 import satori, { type SatoriOptions } from "satori";
 import { html } from "satori-html";
 
+const fontFileRegular = await fetch(
+	"https://cdn.jsdelivr.net/npm/@openfonts/noto-sans-jp_japanese@1.44.5/files/noto-sans-jp-japanese-500.woff",
+);
+
+// fontFileRegular から ArrayBuffer を取得
+const fontRegularArrayBuffer = await fontFileRegular.arrayBuffer();
+
+console.log(fontRegularArrayBuffer);
+
 const ogOptions: SatoriOptions = {
 	// debug: true,
 	fonts: [
 		{
-			data: Buffer.from(RobotoMono),
-			name: "Roboto Mono",
+			data: Buffer.from(fontRegularArrayBuffer),
+			name: "NotoSansJP",
 			style: "normal",
-			weight: 400,
-		},
-		{
-			data: Buffer.from(RobotoMonoBold),
-			name: "Roboto Mono",
-			style: "normal",
-			weight: 700,
+			weight: 500,
 		},
 	],
 	height: 630,
 	width: 1200,
 };
 
+const formatDate = (dateString: string) => {
+	const date = new Date(dateString);
+	const yyyy = date.getFullYear();
+	const mm = String(date.getMonth() + 1).padStart(2, "0");
+	const dd = String(date.getDate()).padStart(2, "0");
+	return `${yyyy}/${mm}/${dd}`;
+};
+
 const markup = (title: string, pubDate: string) =>
-	html`<div tw="flex flex-col w-full h-full bg-white text-[#1d1f21]" style="border: 16px solid #2bbc89;">
-	  <div tw="flex flex-col flex-1 w-full p-10 justify-center">
-		<h3 tw="text-4xl mb-6">${pubDate}</h3>
-		<h1 tw="text-8xl font-bold leading-snug text-black">${title}</h1>
-	  </div>
-	  <div tw="flex items-center justify-between w-full p-10 text-xl">
-		<div tw="flex items-center">
-		  <img src="https://pbs.twimg.com/profile_images/1692602187795554304/KOy7bS--_400x400.png" width="100" height="100" tw="object-cover rounded-full" alt="Dokudami">
-		  <h1 tw="text-7xl ml-5 font-semibold text-black">${siteConfig.title}</h1>
+	html`<div
+		tw="flex flex-col w-full h-full bg-white text-[#1d1f21]"
+		style="border: 32px solid #2bbc89; font-family: 'Noto Sans JP', sans-serif;"
+	>
+		<div tw="flex flex-col flex-1 w-full p-10 justify-center ">
+			<h1 tw="text-6xl font-bold leading-snug text-black text-center mx-auto mt-40">${title}</h1>
+			<h3 tw="text-3xl mb-6 text-center mx-auto">${formatDate(pubDate)}</h3>
 		</div>
-	  </div>
+		<div tw="flex items-center justify-between w-full p-10 text-xl">
+			<div tw="flex items-center">
+				<img
+					src="https://pbs.twimg.com/profile_images/1692602187795554304/KOy7bS--_400x400.png"
+					width="50"
+					height="50"
+					tw="object-cover rounded-full"
+					alt="Dokudami"
+				/>
+				<h1 tw="text-4xl ml-5 font-semibold text-black">${siteConfig.title}</h1>
+			</div>
+		</div>
 	</div>`;
-  
 
 type Props = InferGetStaticPropsType<typeof getStaticPaths>;
 
