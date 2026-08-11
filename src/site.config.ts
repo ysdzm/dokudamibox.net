76459b3448e8f3dc146bab2f7ed51dc8e29e00d4
dokudami <ysdzm75@gmail.com>
2026-08-11 16:02:28 +0000

    Update site description

--- src/site.config.ts
+++ src/site.config.ts
@@ -5,7 +5,7 @@ export const siteConfig: SiteConfig = {
 	// Used as both a meta property (src/components/BaseHead.astro L:31 + L:49) & the generated satori png (src/pages/og-image/[slug].png.ts)
 	author: "dokudami",
 	// Meta property used as the default description meta property
-	description: "An opinionated starter theme for Astro",
+	description: "dokudamibox.netからはじめよう、",
 	// HTML lang property, found in src/layouts/Base.astro L:18
 	lang: "en-GB",
 	// Meta property, found in src/components/BaseHead.astro L:42
